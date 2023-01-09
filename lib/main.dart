63e5f7b0d375b0f393f606e69315058a98011f5a
Daniel <danielmtdc16@gmail.com>
2023-01-09 09:54:08 -0300

    Criando as condições e implementando um Form.

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,33 @@ class _MyAppState extends State<MyApp> {
   final heightController = TextEditingController();
   final weightController = TextEditingController();
 
+  final _formkey = GlobalKey<FormState>();
+
   void calculate(){
     double height = double.parse(heightController.text)/100;
     double weight = double.parse(heightController.text);
 
     setState(() {
-      result = (weight / (height * height)).toStringAsFixed(2);
+      double imc = weight / (height * height);
+
+      if (imc < 18.5){
+        result = "Magreza. Imc = ${imc.toStringAsFixed(2)}";
+      }
+      else if (imc >= 18.5 && imc <= 24.9){
+        result = "Normal. Imc = ${imc.toStringAsFixed(2)}";
+      }
+      else if (imc >= 25 && imc <= 29.9){
+        result = "Sobrepeso. Imc = ${imc.toStringAsFixed(2)}";
+      }
+      else if (imc >= 30 && imc <= 34.9){
+        result = "Obesidade Grau I. Imc = ${imc.toStringAsFixed(2)}";
+      }
+      else if (imc >= 35 && imc <= 39.9){
+        result = "Obesidade Grau II. Imc = ${imc.toStringAsFixed(2)}";
+      }
+      else if (imc > 40){
+        result = "Obesidade Grau III. Imc = ${imc.toStringAsFixed(2)}";
+      }
     });
   }
 
@@ -38,36 +59,39 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Padding(
         padding: const EdgeInsets.only(right: 20, left: 20, top: 40),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            buildTextField("Altura", "cm", heightController),
-            const SizedBox(height: 20,),
-            buildTextField("Peso", "kg", weightController),
-            const SizedBox(height: 20,),
-            ElevatedButton(
-                onPressed: calculate,
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(0, 50)
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildTextField("Altura", "cm", heightController),
+              const SizedBox(height: 20,),
+              buildTextField("Peso", "kg", weightController),
+              const SizedBox(height: 20,),
+              ElevatedButton(
+                  onPressed: calculate,
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(0, 50)
+                  ),
+                  child: const Text("Calcular", style: TextStyle(fontSize: 19),)
+              ),
+              const SizedBox(height: 20,),
+              Container(
+                height: 50,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.blue
                 ),
-                child: const Text("Calcular", style: TextStyle(fontSize: 19),)
-            ),
-            const SizedBox(height: 20,),
-            Container(
-              height: 50,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Colors.blue
-              ),
-              child: Center(child: Text(result,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 19
-                ),),
-              ),
-            )
-          ],
+                child: Center(child: Text(result,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 19
+                  ),),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -75,7 +99,7 @@ class _MyAppState extends State<MyApp> {
 }
 
 Widget buildTextField(String labelText, String suffixText, TextEditingController controller){
-  return TextField(
+  return TextFormField(
     controller: controller,
     decoration: InputDecoration(
       labelText: labelText,
