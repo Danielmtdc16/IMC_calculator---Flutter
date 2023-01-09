707a09911e528a734dc6978d9db9a57ce77e1ca4
Daniel <danielmtdc16@gmail.com>
2023-01-09 17:05:50 -0300

    Implementando o design.

--- lib/imc_calculator_2/tela_principal.dart
+++ lib/imc_calculator_2/tela_principal.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const Color corAtivaCard = Color(0xFF9E9E9E);
+const Color corContainerInferior = Color(0xFFFF5822);
+const double alturaContainerInferior = 80.0;
 
 class TelaPrincipal extends StatefulWidget {
   const TelaPrincipal({Key? key}) : super(key: key);
@@ -20,39 +23,82 @@ class _TelaPrincipalState extends State<TelaPrincipal> {
       body: Column(
         children: [
           Expanded(child: Row(
-            children: [
+            children: const [
               Expanded(child: Card(
                 cor: corAtivaCard,
+                filhoCard: ConteudoIcone(
+                  icone: FontAwesomeIcons.mars,
+                  descricao: "MASCULINO",
+                ),
               )),
               Expanded(child: Card(
                 cor: corAtivaCard,
+                filhoCard: ConteudoIcone(
+                    icone: FontAwesomeIcons.venus,
+                    descricao: "FEMININO"
+                ),
               )),
             ],
           )),
           Expanded(child: Card(
             cor: corAtivaCard,
+            filhoCard: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text("asdasd")
+              ],
+            ),
           )),
           Expanded(child: Row(
             children: [
               Expanded(child: Card(
                 cor: corAtivaCard,
+                  filhoCard: Text("")
               )),
               Expanded(child: Card(
                 cor: corAtivaCard,
+                  filhoCard: Text("")
               )),
             ],
           )),
+          Container(
+            color: corContainerInferior,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: alturaContainerInferior,
+          )
         ],
       ),
     );
   }
 }
 
+class ConteudoIcone extends StatelessWidget {
+
+  const ConteudoIcone({Key? key, required this.icone, required this.descricao}) : super(key: key);
+
+  final IconData icone;
+  final String descricao;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icone, size: 70,),
+        const SizedBox(height: 10,),
+        Text(descricao, style: const TextStyle(fontSize: 20, color: Colors.black),)
+      ],
+    );
+  }
+}
+
 class Card extends StatelessWidget {
 
-  Card({required this.cor});
+  const Card({Key? key, required this.cor, required this.filhoCard}) : super(key: key);
 
   final Color cor;
+  final Widget filhoCard;
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +108,16 @@ class Card extends StatelessWidget {
         color: cor,
         borderRadius: BorderRadius.circular(10),
       ),
+      child: filhoCard,
     );
+  }
+}
+
+class asd extends StatelessWidget {
+  const asd({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
